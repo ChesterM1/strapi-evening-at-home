f9f4de06c90bad4520dbe1d746dabd5f3d006cf4
AlexandrM <chestermen30@gmail.com>
2023-03-19 00:37:11 +0200

    add yaml

--- config/database.js
+++ config/database.js
@@ -1,26 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "sqlite",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      filename: env(
+        "DATABASE_FILENAME",
+        path.join(__dirname, "..", ".tmp/data.db")
+      ),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
